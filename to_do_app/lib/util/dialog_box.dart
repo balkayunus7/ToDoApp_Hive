d92e4e81ef6ad753ddd21dd417116b6e3b78090e
YUNUS BALKA <98759759+balkayunus7@users.noreply.github.com>
2023-06-26 04:16:02 +0300

    Slidable and delete tasks added

--- to_do_app/lib/util/dialog_box.dart
+++ to_do_app/lib/util/dialog_box.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/util/my_button.dart';
 
+// ignore: must_be_immutable
 class DialogBox extends StatelessWidget {
   VoidCallback onSave;
   VoidCallback onCancel;
